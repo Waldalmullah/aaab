9b25d170f87a58df475769524cbf4e19aef07206
Waldalmullah <85621847+Waldalmullah@users.noreply.github.com>
2022-12-22 16:17:09 +0300

    Adding Chatbot UI (need button and body) *I dont know how to do it*

--- lib/ui/views/chat_bot/chat_bot_view.dart
+++ lib/ui/views/chat_bot/chat_bot_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../app/theme/AppColors.dart';
 import './chat_bot_view_model.dart';
 
 class ChatBotView extends StatelessWidget {
@@ -13,10 +15,63 @@ class ChatBotView extends StatelessWidget {
       onModelReady: (ChatBotViewModel model) async => await model.init(),
       builder: (BuildContext context, ChatBotViewModel model, Widget? child) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'ChatBotView',
-            ),
+          appBar: AppBar(
+            leading: Stack(children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: IconButton(
+                    onPressed: () => model.goBack(),
+                    icon: const Icon(Icons.arrow_back)),
+              )
+            ]),
+            title: const Text('Chatbot'),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            foregroundColor: AppColors.maroni,
+            elevation: 0,
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Center(
+                  child: Container(
+                    child: Text(
+                      'Today, ${DateFormat("Hm").format(DateTime.now())}',
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.maroni),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: AppColors.maroni,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none),
+                      filled: true,
+                      fillColor: const Color(0xff808080).withOpacity(.12),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none)),
+                ),
+              ),
+            ],
           ),
         );
       },
